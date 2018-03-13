9dcd6ea15c41c6d63c7b0340a5ad67d816382e7f
Chiba9 <2032917388@qq.com>
2018-03-13 13:53:33 +0800

    选手包改名适应背景

--- 51CF_logic/logic/player_code.h
+++ 51CF_logic/logic/player_code.h
@@ -1,10 +1,10 @@
 #pragma once
-/* XiangGuHuaJi 2016, player.h
+/* DAGAN 2017, player_code.h
 *
 */
 
-#ifndef _XIANGGUHUAJI_PLAYER_H__
-#define _XIANGGUHUAJI_PLAYER_H__
+#ifndef _DAGAN_PLAYER_CODE_H__
+#define _DAGAN_PLAYER_CODE_H__
 
 
 #include<string>
