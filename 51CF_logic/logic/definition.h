f8d4312cf402cacb014ba20012c58fc10733731b
Director-of-G <jyp19@mails.tsinghua.edu.cn>
2021-01-24 14:32:40 +0800

    add new definition in FC18

--- 51CF_logic/logic/definition.h
+++ 51CF_logic/logic/definition.h
@@ -14,6 +14,14 @@
 #include <iostream>
 
 using namespace std;
+typedef int    TMovePoint;     //【FC18】行动力
+typedef int    TBattlePoint;   //【FC18】战斗力
+typedef int    THealthPoint;   //【FC18】生命值
+typedef int    TBuildPoint;    //【FC18】劳动力：工程建设兵团属性
+typedef int    TProductPoint;  //【FC18】塔的生产力
+typedef int    TOccupyPoint;   //【FC18】塔对周围方格施加的占有属性值
+typedef int    TIntPara;       //【FC18】整型系数值
+typedef double TDoublePara;    //【FC18】双精度浮点型系数值
 typedef double TSpeed;
 typedef double TResourceD;  //double 的资源数，用于内部操作
 typedef int    TResourceI;  //int    的资源数，用于显示
@@ -33,6 +41,12 @@ typedef int    TRound;  //回合数
 typedef double TPower;  //倍率
 typedef int    TTentacleNum;
 
+const int MAX_STARLEVEL = 3;    //【FC18】最大的兵团等级
+const int TERRAIN_TYPE_NUM = 6;    //【FC18】地形的种类数
+const int BATTLE_CORPS_TYPE_NUM = 3;    //【FC18】作战兵团的种类数
+const int CONSTRUCT_CORPS_TYPE_NUM = 2;    //【FC18】工程兵团的种类数
+const int TOWER_PRODUCT_TASK_NUM = 6;     //【FC18】塔的生产任务种类数
+const int OCCUPY_POINT_DIST_SCALE = 5;    //【FC18】塔对周围方格施加占有属性值的距离等级有几个
 
 //兵力密度
 const double       Density = 0.1;
@@ -65,6 +79,80 @@ const TResourceD ExtendingSpeedUpdateCost[MAX_EXTENDING_SPEED_LEVEL] = { 2,4,6,8
 const TResourceD ExtraControlStageUpdateCost[MAX_EXTRA_CONTROL_LEVEL] = { 3,5,7 };
 const TResourceD DefenceStageUpdateCost[MAX_DEFENCE_LEVEL] = { 3,5,7 };
 
+
+//【FC18】兵团种类
+enum corpsType
+{
+	Battle = 0,          //作战兵团
+	Construct = 1        //工程建设兵团
+};
+
+
+//【FC18】作战兵团种类的枚举类
+enum battleCorpsType
+{
+	Warrior = 0,         //战士
+	Archer  = 1,         //弓箭手
+	Cavalry = 2,         //骑兵
+};
+
+
+//【FC18】工程建设兵团的枚举类
+enum constructCorpsType
+{
+	Builder = 0,           //建造者
+	Extender = 1           //开拓者
+};
+
+
+//【FC18】作战兵团操作类型（C前缀表示Corps）
+enum corpsCommand
+{//                                                          作战兵团        工程兵团
+	CMove          = 0,       //在地图上移动                     √              √
+	CStation       = 1,       //驻扎在地图方格（无自己塔）       √              √
+	CStationTower  = 2,       //驻扎在塔（自己势力的塔）         √              √
+	CAttackCorps   = 3,       //攻击对方势力的兵团               √
+	CAttackTower   = 4,       //攻击对方势力的塔                 √
+	CRegroup       = 5,       //兵团整编                         √
+	CDissolve      = 6,       //兵团解散                         √              √
+	CBuild         = 7,       //修建新防御塔                                     √
+	CRepair        = 8,       //修理原防御塔                                     √
+	CChangeTerrain = 9,       //改变方格地形                                     √
+};
+
+
+//【FC18】塔的操作类型（T前缀表示tower）
+enum towerCommand
+{
+	TProduct       = 0,       //生产任务
+	TAttack        = 1        //攻击任务
+};
+
+
+//【FC18】塔的生产任务类型（P表示product）
+enum productType
+{//                                                 生产回报
+	PWarrior       = 0,       //生产战士         1star-战士兵团
+	PArcher        = 1,       //生产弓箭手      1star-弓箭手兵团
+	PCavalry       = 2,       //生产骑兵         1star-骑兵兵团
+	PBuilder       = 3,       //生产建造者        1-建造者兵团
+	PExtender      = 4,       //生产开拓者        1-开拓者兵团
+	PUpgrade       = 5        //塔升级任务      塔等级+1（max=8)
+};
+
+
+//【FC18】地形的枚举类
+enum terrainType
+{
+	Tower = 0,       //塔
+	Plain = 1,       //平原
+	Mountain = 2,       //山地
+	Forest = 3,       //森林
+	Swamp = 4,       //沼泽
+	Road = 5,       //道路
+};
+
+
 enum CellStrategy
 {
 	Normal    //初始状态
@@ -72,6 +160,117 @@ enum CellStrategy
 	, Defence //防御
 	, Grow    //发育
 };
+
+
+//【FC18】作战兵团行动力（与作战兵团的枚举类在序号上对应，且考虑了等级）
+const TMovePoint battleMovePoint[BATTLE_CORPS_TYPE_NUM][MAX_STARLEVEL]
+{
+	{2,   2,   2},    //战士
+	{2,   2,   2},    //弓箭手
+	{4,   4,   4}     //骑兵
+};
+
+//【FC18】作战兵团初始战斗力（与作战兵团的枚举类在序号上对应，且考虑了等级）
+const TBattlePoint corpsBattlePoint[BATTLE_CORPS_TYPE_NUM][MAX_STARLEVEL]
+{
+	{36,   44,   52},    //战士
+	{30,   38,   46},    //弓箭手
+	{44,   52,   60}     //骑兵
+};
+
+
+//【FC18】作战兵团的战斗力增益系数（与作战兵团的枚举类在序号上对应，且考虑了等级）
+const TIntPara corpsBattleGain[BATTLE_CORPS_TYPE_NUM][MAX_STARLEVEL]
+{
+	{2,   2,   2},    //战士
+	{2,   2,   2},    //弓箭手
+	{4,   4,   4}     //骑兵
+};
+
+
+//【FC18】作战兵团的攻城系数（与作战兵团的枚举类在序号上对应，且考虑了等级）
+const TDoublePara corpsAttackTower[BATTLE_CORPS_TYPE_NUM][MAX_STARLEVEL]
+{
+	{0.4,   0.4,   0.4},    //战士
+	{0.7,   0.7,   0.7},    //弓箭手
+	{0.5,   0.5,   0.5}     //骑兵
+};
+
+
+//【FC18】作战兵团初始生命值（与作战兵团的枚举类在序号上对应，且考虑了等级）
+const THealthPoint battleHealthPoint[BATTLE_CORPS_TYPE_NUM][MAX_STARLEVEL]
+{
+	{60,   80,   90},    //战士
+	{50,   70,   90},    //弓箭手
+	{70,   90,   110}    //骑兵
+};
+
+
+//【FC18】工程建设兵团操作的劳动力消耗（与兵团操作的枚举类在序号上对应
+const TBuildPoint constructBuildCost
+{
+	0,       //在地图上移动
+	0,       //驻扎在地图方格（无自己塔）
+	0,       //驻扎在塔（自己势力的塔）
+	0,       //攻击对方势力的兵团
+	0,       //攻击对方势力的塔
+	0,       //兵团整编
+	0,       //兵团解散
+	0,       //修建新防御塔
+	1,       //修理原防御塔
+	1,       //改变方格地形
+};
+
+
+//【FC18】塔的生产任务生产力消耗值
+const TProductPoint TowerProductCost[TOWER_PRODUCT_TASK_NUM]
+{//                                 生产回报                                           特殊说明
+	40,       //生产战士         1star-战士兵团
+	60,       //生产弓箭手      1star-弓箭手兵团
+	100,      //生产骑兵         1star-骑兵兵团
+	40,       //生产建造者        1-建造者兵团
+	40,       //生产开拓者        1-开拓者兵团
+	40        //塔升级任务      塔等级+1（max=8)       这里40是最小值，实际值为执行该任务的第一回合塔的等级*40，要根据实际情况算  
+};
+
+
+//【FC18】塔对周围方格施加的占有属性值，与距离的关系表
+const TOccupyPoint TowerOccupyPoint[OCCUPY_POINT_DIST_SCALE + 1]
+{//                方格跟塔的距离
+	100,//              1格
+	80,//               2格
+	50,//               3格
+	20,//               4格
+	10,//               5格
+	0//               6格或更远
+};
+
+
+//【FC18】行动力消耗（与地形的枚举类在序号上对应）
+const TMovePoint CorpsMoveCost[TERRAIN_TYPE_NUM + 1]
+{
+	0,    //塔
+	2,    //平原
+	4,    //山地
+	3,    //森林
+	4,    //沼泽
+	1,    //道路
+	0     //多余的，用于后续扩展
+};
+
+
+//【FC18】战斗力增益（与地形的枚举类在序号上对应）
+const TBattlePoint CorpsBattleGain[TERRAIN_TYPE_NUM + 1]
+{
+	0,    //塔
+	0,    //平原
+	5,    //山地
+	3,   //森林
+	-3,    //沼泽
+	0,    //道路
+	0     //多于的，用于后续扩展
+};
+
 
 //细胞策略改变花费科技点
 const TTechPoint CellChangeCost[4][4] =
