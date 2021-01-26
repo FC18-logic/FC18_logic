6d684a0cbfa10610435f805dd1d8c2dbd9cd879a
刘雪洁 <@DESKTOP-RFVN389>
2021-01-26 21:35:49 +0800

    塔修改

--- 51CF_logic/logic/tower.cpp
+++ 51CF_logic/logic/tower.cpp
@@ -2,14 +2,24 @@
 
 TTowerID Tower::ID = 0;
 
-Tower::Tower(void)
+Tower::Tower(void) :m_data(nullptr)
 {
 }
 Tower::~Tower(void)
 {
 }
 //构造函数：塔属性初始化
-
+Tower::Tower(DATA::Data* _data, TPlayerID m_playid, TPoint pos) :m_data(_data)
+{
+	m_PlayerID = m_playid;
+	m_position = pos;
+	m_id = ID;
+	ID++;
+	m_level = 1;//初始等级为1  
+	set_all(m_level);
+	m_experpoint = 0;//初始经验值为0
+	m_productconsume = INF;
+}
 /*
 名称：level_upgrade
 功能：根据当前经验值更新等级
@@ -42,9 +52,28 @@ bool Tower::set_level()
 	return upgrade;
 }
 /*
+名称：product_crops
+功能：生产兵团
+参数：待生产兵团种类
+by lxj
+*/
+void Tower::product_crops(productType protype)
+{
+	if (protype == PWarrior) 
+		Crops temp(m_data, Battle, Warrior, Builder, m_PlayerID, m_position);
+	if (protype == PArcher)
+		Crops temp(m_data, Battle, Archer, Builder, m_PlayerID, m_position);
+	if (protype == PCavalry)
+		Crops temp(m_data, Battle, Cavalry, Builder, m_PlayerID, m_position);
+	if (protype == PBuilder)
+		Crops temp(m_data, Construct, Warrior, Builder, m_PlayerID, m_position);
+	if (protype == PExtender)
+		Crops temp(m_data, Construct, Warrior, Extender, m_PlayerID, m_position);
+}
+/*
 名称：set_producttype
 功能：结算上一回合生产任务完成情况/设置新的生产任务
-参数：待设置生产任务    *待补充（若为生产兵团任务还需传入兵团种类）
+参数：待设置生产任务 
 返回值：上一回合生产任务是否完成
 by lxj
 */
@@ -55,10 +84,9 @@ bool Tower::set_producttype(productType m_protype)
 	if (m_productconsume <= 0) //上一回合生产任务完成
 	{
 		finish = true;
-		if (m_producttype == 0) //当前任务为生产兵团      *待补充
-		{
-		}
-		if (m_producttype == 1)//当前任务为升级项目
+		if (m_producttype < 5) //当前任务为生产兵团    
+			product_crops(m_producttype);
+		if (m_producttype == PUpgrade)//当前任务为升级项目
 		{
 			m_level += 1;//完成后塔上升一级
 			if (m_level > MAX_TOWER_LEVEL) //不得超过最大等级
@@ -78,17 +106,17 @@ bool Tower::set_producttype(productType m_protype)
 	}
 	m_producttype = m_protype;
 	//更新新一回合生产任务的生产力消耗值
-	if (m_producttype == 0)
+	if (m_producttype < 5)
 	{
-		//m_productconsume = TowerProductCost[兵团种类];
+		m_productconsume = TowerProductCost[m_producttype];
 		m_productconsume -= m_productpoint;
 	}
-	if (m_producttype == 1)
+	if (m_producttype == PUpgrade)
 	{
 		m_productconsume = 40 * m_level;
 		m_productconsume -= m_productpoint;
 	}
-	return finish;
+	return finish; 
 }
 
 
--- 51CF_logic/logic/tower.h
+++ 51CF_logic/logic/tower.h
@@ -15,35 +15,37 @@ private:
 	static TTowerID		ID;
 	TTowerID			m_id;//塔ID
 	TProductPoint       m_productpoint;//生产力
-	productType			m_producttype;//生产任务类型
-	int					m_towercommand;//操作任务类型  0：生产	1：升级	   *补充定义
+	productType			m_producttype;//生产任务类型  0-4：生产	5：升级
 	THealthPoint        m_healthpoint;//生命值
 	TBattlePoint        m_battlepoint;//战斗力
 	TExperPoint			m_experpoint;//经验值
 	TExperPoint			m_upgradexper;//升级到下一级所需经验值
 	TRound				m_round;//游戏回合数
-	int					m_level;//等级:从0开始                 *补充定义
+	int					m_level;//等级:从1开始                 *补充定义
 	int					m_attackrange;//攻击范围               *补充定义
 	int					m_productconsume;//当前生产任务对应得生产力消耗值	*补充定义
 
 	TPlayerID			m_PlayerID;//所属阵营
 
 	TPoint				m_position;//位置
+
+	DATA::Data* const		m_data;//游戏信息
 	//根据等级更新塔的属性
 	void set_all(int level) 
 	{
-		m_productpoint = TowerInitConfig[level].initBuildPoint;
-		m_healthpoint = TowerInitConfig[level].initHealthPoint;
-		m_battlepoint = TowerInitConfig[level].initProductPoint;
-		m_upgradexper = TowerInitConfig[level].upgradeExper;
-		m_attackrange = TowerInitConfig[level].battleRegion;
+		m_productpoint = TowerInitConfig[level - 1].initBuildPoint;
+		m_healthpoint = TowerInitConfig[level - 1].initHealthPoint;
+		m_battlepoint = TowerInitConfig[level - 1].initProductPoint;
+		m_upgradexper = TowerInitConfig[level - 1].upgradeExper;
+		m_attackrange = TowerInitConfig[level - 1].battleRegion;
 	}
 	//每回合初始根据经验值判断是否升级
 	bool set_level();
+	void product_crops(productType protype);
 public:
 	Tower(void);
 	~Tower(void);
-	Tower(TPlayerID ID, TPoint pos);
+	Tower(DATA::Data* _data, TPlayerID m_playid, TPoint pos);
 	//每一回合开始时，结算上轮生产任务完成情况，并设置新的生产任务
 	bool set_producttype(productType m_protype);
 	//显示所有者ID
